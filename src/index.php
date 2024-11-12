5cc2aab995aa1eb3f5025c9bf14d3d1390f22ea9
tinaforeby <tina.foreby@gmail.com>
2024-11-12 16:57:14 +0100

    Added code, style and images

--- src/index.php
+++ src/index.php
@@ -6,40 +6,58 @@
 
     <title>Profil</title>
     <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+
 </head>
 <body>
     <section class="profile-card">
         <section class="profile-image">
-            <img src="src/DSC05368.jpg" alt="Foto på mig" loading="lazy">
+            <img src="DSC053681.jpg" class="image" alt="Foto på mig" loading="lazy">
         </section>
 
-        <section class="greeting">
-            <h1> Profil </h1>
-            <?php
-                $name = "Tina Foreby";
-                $age = 29;
-                $city = "Stockholm";
+        <section class="intro">
+            <section class="greeting">
+                <h1> Profil </h1>
+                <?php
+                    $name = "Tina Foreby";
+                    $age = 29;
+                    $city = "Stockholm";
 
-                function greeting() {
-                    global $name;
-                    echo "Välkommen, $name <br>";
-                }
-
-                greeting();
-
-                echo "Du är " . $age . " år gammal och du bor i " . $city . ".";
-            ?>
-        </section>
-
-        <section class="ofAge">
-            <p> Status: </p>
-            <?php
-                if ($age >= 18) {
-                    echo "Du är myndig <br>";
-                } else { 
-                    echo "Du är inte myndig <br>";
+                    function greeting() {
+                        global $name;
+                        echo '<p class="greeting-text">Välkommen, ' . $name . '</p>';
                     }
-            ?>
+
+                    greeting();
+
+                    echo "Du är " . $age . " år gammal och du bor i " . $city . ".";
+                ?>
+            </section>
+
+            <section class="off-age">
+                <p> Status: </p>
+                <?php
+                    if ($age >= 18) {
+                        echo "Du är myndig <br>";
+                    } else { 
+                        echo "Du är inte myndig <br>";
+                        }
+                ?>
+            </section>
+
+            <section class="occupation"> 
+                <p> Sysselsättning: </p>
+                <?php
+                    $occupation = "Studerar";
+                    if ($occupation == "Studerar") {
+                        echo "Du $occupation Utveckling på Chas Academy";
+                    }
+                    else {
+                        echo "Du $occupation som Utvecklare"; 
+                    }
+                ?>
+
+            </section>
         </section>
         
         <section class="hobbies">
